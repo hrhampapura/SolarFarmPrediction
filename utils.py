660bac2eb32b310794f1db618af46be595a09498
akhadse <akhadse@go.olemiss.edu>
2023-11-09 13:42:18 -0600

    updated demand function to include km units

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import geopy.distance
 #________________________________________________________________________________
 # Distance in miles.Default value = 50 miles 
 # Returns demand in Megawatthours
-def get_demand_within_distance(latitude,longitude,demand_within_distance=50):
+def get_demand_within_distance(latitude,longitude,demand_within_distance=50,unit='km'):
     data_california = pd.read_csv('demand_data_california_with_lat_lon.csv')
     coords_1 = (latitude,longitude)
     demand = 0.
@@ -17,7 +17,11 @@ def get_demand_within_distance(latitude,longitude,demand_within_distance=50):
         lon = data_california.at[ind,'Longitude']
         if not (np.isnan(lat) and np.isnan(lon)):
             coords_2 = (lat,lon)
-            d = geopy.distance.geodesic(coords_1, coords_2).miles
+            if unit=='mile':
+            	d = geopy.distance.geodesic(coords_1, coords_2).miles
+            elif unit=='km':
+            	d = geopy.distance.geodesic(coords_1, coords_2).km
+            	
             if d<=demand_within_distance:
                 demand += data_california.at[ind,'Sales (Megawatthours)']
                 
